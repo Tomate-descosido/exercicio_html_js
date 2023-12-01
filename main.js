def7703502e7f7ece6c911272c669f04350b013e
Eduardo O D <edudobbinshotmail.com@gmail.com>
2023-12-01 19:46:25 -0400

    correção no código javascript

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const form = document.getElementById('formulário')
 
 form.addEventListener('submit', function(e){
     e.preventDefault()
-    var númeroA = document.getElementById('A')
-    var númeroB = document.getElementById('B')
-    if(númeroA.value > númeroB.value){
+    var númeroA = parseFloat(document.getElementById('A').value)
+    var númeroB = parseFloat(document.getElementById('B').value)
+    if(númeroA > númeroB){
         A.style.border = '2px solid red'
         B.style.border = '2px solid red'
         alert("O número B não é maior que o número A");}
